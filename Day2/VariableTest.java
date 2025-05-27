d388fda336e3a9c084cc6773eaa4d91ff448005f
wuhm72 <wuhm72@chinaunicom.com>
2025-05-27 19:51:05 +0800

    feat: 添加VariableTest类的主方法和变量计算功能
    添加了main方法实现变量i和j的声明、相加操作，并打印结果k。这是为了演示基本的变量操作和输出功能。

--- Day2/VariableTest.java
+++ Day2/VariableTest.java
@@ -1,3 +1,8 @@
-class VariableTest{
-
+class VariableTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+        System.out.println(k);
+    }
 }
