a0c43bb1f6671affd9bf14dda4ffc88a3199ef15
hm-harry <hmhurry@163.com>
2025-06-07 22:47:27 +0800

    feat: 添加查找数组最小值索引的方法
    在Demo1类中新增minIndex方法用于查找数组中最小值的索引，并更新main方法进行测试。这将提供基本的数组操作功能。

--- Day7/src/com/qfedu/Demo1.java
+++ Day7/src/com/qfedu/Demo1.java
@@ -7,7 +7,25 @@ public class Demo1 {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("你好");
-        
+        int arr[] = {1, 3, 2, -1, 0, 5, 6, 4};
+        int minIndex = minIndex(arr);
+        System.out.println("min index: " + minIndex);
+    }
+
+    /**
+     * 求数组中最小值的索引
+     *
+     * @param arr int类型数组
+     * @return 最小值所在的下标位置
+     * 
+     * */
+    public static int minIndex(int[] arr) {
+        int minIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < arr[minIndex]) {
+                minIndex = i;
+            }
+        }
+        return minIndex;
     }
 }
